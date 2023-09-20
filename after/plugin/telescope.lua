13c5e192ea393050c0d3eb88530adf2e3506e86f
Juan Manuel Barreneche <jumanbar@gmail.com>
2023-09-20 00:30:16 -0300

    Update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,14 +1,19 @@
+local builtin = require('telescope.builtin');
 
--- Titulo ====
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>gs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+vim.keymap.set('n', '<C-p>',      builtin.git_files, {});
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {});
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {});
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {});
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {});
+vim.keymap.set('n', '<leader>fr', builtin.treesitter, {});
+
+vim.keymap.set('v', '<leader>gs', builtin.grep_string, {});
+vim.keymap.set("n", "<leader>gs", function()
+    local text = vim.fn.expand("<cword>")
+    builtin.grep_string({ search = text })
 end)
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-
-
+vim.keymap.set("n", "<leader>gS", function()
+    local text = vim.fn.expand("<cWORD>")
+    builtin.grep_string({ search = text })
+end)
